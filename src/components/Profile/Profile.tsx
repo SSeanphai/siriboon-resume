7dddadfc87b0c71aec7e1c8369a83bea81d5cfc0
Seanphai <binggopok@gmail.com>
2024-05-08 14:06:12 +0700

    img fix

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -51,7 +51,7 @@ const Profile: FC = () => {
       <div className="profile_image">
         <img
           className="image"
-          src="https://media.discordapp.net/attachments/813807614462263336/1104086455099326514/A4F76F81-9D9F-43F6-A768-16E39D6329FF.jpg?ex=6635bd63&is=66346be3&hm=9354635e17bd6f074523b70766114b74e2120c759f3aa929bda22082bd510ac9&=&format=webp&width=550&height=550"
+          src="https://i.ibb.co/NywbjK6/A4-F76-F81-9-D9-F-43-F6-A768-16-E39-D6329-FF.jpg"
           alt=""
         />
       </div>
